638e16326aedef4dbd5417623226a7c02361e5ed
Mobil Game 06 <nico.schnass@gmail.com>
2022-02-15 16:50:09 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+hostname = "192.168.178.9:8006" #The proxmox url(local/public ip or domain) with port
+user = "user@pve"               #username with @
+password = "secret_pw"          #the password of the account
+verify_ssl = False              #if you don't want to check ssl
